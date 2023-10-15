12c44d85e270f6aea2854d7fd6dfcd33a224e42f
shmsacr <shmsacr@gmail.com>
2023-10-15 19:20:39 +0300

    feat add block event and load user

---
+++ lib/src/core/contacts_bloc/app_event.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+
+@immutable
+abstract class UserEvent extends Equatable {}
+
+class LoadUserEvent extends UserEvent {
+  @override
+  List<Object?> get props => [];
+}
