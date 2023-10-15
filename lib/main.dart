49f30c316410ee7b0b022fdeba7791411f7a25ad
shmsacr <shmsacr@gmail.com>
2023-10-15 19:23:07 +0300

    feat add bloc repository

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:contacts_app/src/core/model/customUser.dart';
 import 'package:contacts_app/src/core/model/user_database_provider.dart';
-import 'package:contacts_app/src/screens/login/home_screen.dart'; // Import your HomeScreen
+import 'package:contacts_app/src/core/repository/contacts/user_repository.dart';
+import 'package:contacts_app/src/screens/home/home_screen.dart'; // Import your HomeScreen
 import 'package:contacts_app/src/screens/login/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   UserDatabaseProvider databaseProvider = UserDatabaseProvider();
   await databaseProvider.open(); // Initialize the database
-
   List<CustomUser> users = await databaseProvider.getAllUsers();
   Widget homeScreen = users.isNotEmpty ? HomeScreen() : LoginScreen();
 
-  runApp(MyApp(homeScreen: homeScreen));
+  runApp(
+    RepositoryProvider<UserRepository>(
+        create: (context) {
+          return UserRepository();
+        },
+        child: MyApp(homeScreen: homeScreen)),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -23,9 +30,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: homeScreen,
-      ),
+      debugShowCheckedModeBanner: false,
+      home: homeScreen,
     );
   }
 }
