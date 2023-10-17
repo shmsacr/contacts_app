4748330c66bed12e5facdaa0514942bd00793692
shmsacr <shmsacr@gmail.com>
2023-10-17 22:25:27 +0300

    feat: create post user request

--- lib/src/core/repository/contacts/user_repository.dart
+++ lib/src/core/repository/contacts/user_repository.dart
@@ -1,3 +1,4 @@
+import 'package:contacts_app/src/core/model/customUser.dart';
 import 'package:dio/dio.dart';
 
 import '../../model/contacts.dart';
@@ -5,13 +6,13 @@ import '../../model/contacts.dart';
 class UserRepository {
   String endpoint = "http://www.motosikletci.com/api/kisiler";
 
-  Future<List<Contacts>> getUsers(String email, String sifre) async {
+  Future<List<Contacts>> getUsers(CustomUser user) async {
     Dio dio = Dio();
     Response response = await dio.post(
       endpoint,
       data: {
-        'email': email,
-        'sifre': sifre,
+        'email': user.email,
+        'sifre': user.sifre,
       },
     );
     if (response.statusCode == 200) {
@@ -26,20 +27,38 @@ class UserRepository {
     }
   }
 
-  Future<void> addUser(String email, String sifre, Contacts contacts) async{
+  Future<void> addUser(CustomUser user, Contacts contacts) async {
     Dio dio = Dio();
-    Response response = await dio.post(
-      endpoint,
-      data: {
-        'email': email,
-        'sifre': sifre,
-        'kisi_ad': contacts.kisi_ad,
-        'kisi_id': contacts.kisi_id,
-        'city_id': contacts.city_id,
-        'town_id': contacts.town_id,
-        'kisi_tel': contacts.kisi_tel,
-      },
 
-    );
+    try {
+      Response response = await dio.post(
+        "http://www.motosikletci.com/api/kisi-kaydet",
+        data: {
+          'email': user.email,
+          'sifre': user.sifre,
+          'kisi_ad': contacts.kisi_ad,
+          'kisi_id': contacts.kisi_id,
+          'city_id': contacts.city_id,
+          'town_id': contacts.town_id,
+          'kisi_tel': contacts.kisi_tel,
+          'resim': contacts.resim,
+        },
+      );
+      if (response.statusCode == 200) {
+        var responseData = response.data;
+        if (responseData['basari'] == 1 && responseData['durum'] == 1) {
+          print('API Response: ${responseData['mesaj']}');
+        } else {
+          throw Exception('API Error: ${responseData['mesaj']}');
+        }
+        print('Contact added successfully: ${response.data}');
+      } else {
+        throw Exception('API Error: ${response.statusMessage}');
+      }
+    } catch (error) {
+      // Handle other errors, for example, network errors
+      print('Error: $error');
+      throw Exception('Error: $error');
+    }
   }
 }
