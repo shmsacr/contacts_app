027a91e077ad98f41b083c673014f615458fd713
shmsacr <shmsacr@gmail.com>
2023-10-17 22:22:37 +0300

    feat: add authention with bloc

--- lib/src/screens/login/login_screen.dart
+++ lib/src/screens/login/login_screen.dart
@@ -1,50 +1,12 @@
 import 'package:contacts_app/src/screens/home/home_screen.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_login/flutter_login.dart';
 
-import '../../core/model/customUser.dart';
-import '../../core/model/user_database_provider.dart';
+import '../../core/bloc/custom_user_bloc/custom_user_bloc.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
-
-  Future<String?> _loginUser(LoginData data) async {
-    try {
-      Dio dio = Dio();
-      Response response = await dio.post(
-        'http://www.motosikletci.com/api/oturum-test',
-        data: {
-          'email': data.name,
-          'sifre': data.password,
-        },
-      );
-
-      if (response.statusCode == 200) {
-        // API'den başarılı yanıt alındı
-        var responseData = response.data;
-        if (responseData['basari'] == 1 && responseData['durum'] == 1) {
-          print('API Response: ${responseData['mesaj']}');
-          CustomUser user = CustomUser(email: data.name, sifre: data.password);
-          UserDatabaseProvider databaseProvider = UserDatabaseProvider();
-          await databaseProvider.open();
-          await databaseProvider.insert(user);
-          await databaseProvider.close();
-        } else {
-          // Handle incorrect API response here, for example:
-          throw Exception('API Error: ${responseData['mesaj']}');
-        }
-      } else {
-        // Handle non-200 status code
-        throw Exception('API Error: ${response.statusMessage}');
-      }
-    } catch (error) {
-      // Handle other errors, for example, network errors
-      print('Error: $error');
-      throw Exception('Error: $error');
-    }
-    return null;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,103 +15,109 @@ class LoginScreen extends StatelessWidget {
       top: Radius.circular(20.0),
     );
 
-    return FlutterLogin(
-      title: 'AREGON',
-      logo: AssetImage('assets/images/logo.png'),
-      onLogin: (loginData) async {
-        try {
-          await _loginUser(loginData);
+    return BlocBuilder<CustomUserBloc, CustomUserState>(
+      builder: (context, state) {
+        return FlutterLogin(
+          title: 'AREGON',
+          logo: AssetImage('assets/images/logo.png'),
+          onLogin: (loginData) async {
+            try {
+              context
+                  .read<CustomUserBloc>()
+                  .add(LoadCustomUserEvent(data: loginData));
 
-          Navigator.of(context).pushAndRemoveUntil(
-            MaterialPageRoute(builder: (_) => const HomeScreen()),
-            (route) => false,
-          );
-        } catch (error) {
-          return 'Kullanıcı adı veya şifre hatalı';
-        }
+              Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(builder: (_) => const HomeScreen()),
+                (route) => false,
+              );
+            } catch (error) {
+              return 'Kullanıcı adı veya şifre hatalı';
+            }
+          },
+          // onSubmitAnimationCompleted: () async {
+          //   Navigator.of(context).pushAndRemoveUntil(
+          //     MaterialPageRoute(builder: (_) => const HomeScreen()),
+          //     (route) => false,
+          //   );
+          // },
+          onRecoverPassword: (_) {
+            return;
+            // Show new password dialog
+          },
+          theme: LoginTheme(
+            primaryColor: Colors.teal,
+            accentColor: Colors.yellow,
+            errorColor: Colors.deepOrange,
+            titleStyle: TextStyle(
+              color: Colors.greenAccent,
+              fontFamily: 'Quicksand',
+              letterSpacing: 4,
+            ),
+            bodyStyle: TextStyle(
+              fontStyle: FontStyle.italic,
+              decoration: TextDecoration.underline,
+            ),
+            textFieldStyle: TextStyle(
+              color: Colors.orange,
+              shadows: [Shadow(color: Colors.yellow, blurRadius: 2)],
+            ),
+            buttonStyle: TextStyle(
+              fontWeight: FontWeight.w800,
+              color: Colors.yellow,
+            ),
+            cardTheme: CardTheme(
+              color: Colors.yellow.shade100,
+              elevation: 5,
+              margin: EdgeInsets.only(top: 15),
+              shape: ContinuousRectangleBorder(
+                  borderRadius: BorderRadius.circular(100.0)),
+            ),
+            inputTheme: InputDecorationTheme(
+              filled: true,
+              fillColor: Colors.purple.withOpacity(.1),
+              contentPadding: EdgeInsets.zero,
+              errorStyle: TextStyle(
+                backgroundColor: Colors.orange,
+                color: Colors.white,
+              ),
+              labelStyle: TextStyle(fontSize: 12),
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.blue.shade700, width: 4),
+                borderRadius: inputBorder,
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.blue.shade400, width: 5),
+                borderRadius: inputBorder,
+              ),
+              errorBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.red.shade700, width: 7),
+                borderRadius: inputBorder,
+              ),
+              focusedErrorBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.red.shade400, width: 8),
+                borderRadius: inputBorder,
+              ),
+              disabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey, width: 5),
+                borderRadius: inputBorder,
+              ),
+            ),
+            buttonTheme: LoginButtonTheme(
+              splashColor: Colors.purple,
+              backgroundColor: Colors.pinkAccent,
+              highlightColor: Colors.lightGreen,
+              elevation: 9.0,
+              highlightElevation: 6.0,
+              shape: BeveledRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+              // shape: CircleBorder(side: BorderSide(color: Colors.green)),
+              // shape: ContinuousRectangleBorder(borderRadius: BorderRadius.circular(55.0)),
+            ),
+          ),
+        );
       },
-      // onSubmitAnimationCompleted: () async {
-      //   Navigator.of(context).pushAndRemoveUntil(
-      //     MaterialPageRoute(builder: (_) => const HomeScreen()),
-      //     (route) => false,
-      //   );
-      // },
-      onRecoverPassword: (_) {
-        return;
-        // Show new password dialog
-      },
-      theme: LoginTheme(
-        primaryColor: Colors.teal,
-        accentColor: Colors.yellow,
-        errorColor: Colors.deepOrange,
-        titleStyle: TextStyle(
-          color: Colors.greenAccent,
-          fontFamily: 'Quicksand',
-          letterSpacing: 4,
-        ),
-        bodyStyle: TextStyle(
-          fontStyle: FontStyle.italic,
-          decoration: TextDecoration.underline,
-        ),
-        textFieldStyle: TextStyle(
-          color: Colors.orange,
-          shadows: [Shadow(color: Colors.yellow, blurRadius: 2)],
-        ),
-        buttonStyle: TextStyle(
-          fontWeight: FontWeight.w800,
-          color: Colors.yellow,
-        ),
-        cardTheme: CardTheme(
-          color: Colors.yellow.shade100,
-          elevation: 5,
-          margin: EdgeInsets.only(top: 15),
-          shape: ContinuousRectangleBorder(
-              borderRadius: BorderRadius.circular(100.0)),
-        ),
-        inputTheme: InputDecorationTheme(
-          filled: true,
-          fillColor: Colors.purple.withOpacity(.1),
-          contentPadding: EdgeInsets.zero,
-          errorStyle: TextStyle(
-            backgroundColor: Colors.orange,
-            color: Colors.white,
-          ),
-          labelStyle: TextStyle(fontSize: 12),
-          enabledBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: Colors.blue.shade700, width: 4),
-            borderRadius: inputBorder,
-          ),
-          focusedBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: Colors.blue.shade400, width: 5),
-            borderRadius: inputBorder,
-          ),
-          errorBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: Colors.red.shade700, width: 7),
-            borderRadius: inputBorder,
-          ),
-          focusedErrorBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: Colors.red.shade400, width: 8),
-            borderRadius: inputBorder,
-          ),
-          disabledBorder: UnderlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey, width: 5),
-            borderRadius: inputBorder,
-          ),
-        ),
-        buttonTheme: LoginButtonTheme(
-          splashColor: Colors.purple,
-          backgroundColor: Colors.pinkAccent,
-          highlightColor: Colors.lightGreen,
-          elevation: 9.0,
-          highlightElevation: 6.0,
-          shape: BeveledRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-          // shape: CircleBorder(side: BorderSide(color: Colors.green)),
-          // shape: ContinuousRectangleBorder(borderRadius: BorderRadius.circular(55.0)),
-        ),
-      ),
     );
   }
 }
