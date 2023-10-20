cc3b09950c174e7e0d0dea7ab580d191e0d2d5a2
shmsacr <shmsacr@gmail.com>
2023-10-20 16:13:37 +0300

    feat: add imports made and service

--- lib/src/core/bloc/city_bloc/city_bloc.dart
+++ lib/src/core/bloc/city_bloc/city_bloc.dart
@@ -1,8 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:contacts_app/src/core/bloc/city_bloc/city_event.dart';
 import 'package:contacts_app/src/core/bloc/city_bloc/city_state.dart';
-
-import '../../repository/city/city_repository.dart';
+import 'package:contacts_app/src/core/data/service/city_service/city_service.dart';
+import 'package:contacts_app/src/core/data/service/city_service/city_service_impl.dart';
 
 class CityBloc extends Bloc<CityEvent, CityState> {
   CityBloc() : super(CityLoadingState()) {
@@ -10,9 +10,9 @@ class CityBloc extends Bloc<CityEvent, CityState> {
       emit(CityLoadingState());
       // TODO: implement event handler
       try {
-        final CityRepository _cityRepository = CityRepository();
+        final CityService _cityService = CityServiceImpl();
 
-        final city = await _cityRepository.getCities();
+        final city = await _cityService.getCities();
         emit(CityLoadedState(city));
       } catch (e) {
         emit(CityErrorState(e.toString()));
