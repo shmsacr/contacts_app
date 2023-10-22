68a3ba1a91c9916d48983a92f54820c1105c6962
shmsacr <shmsacr@gmail.com>
2023-10-22 22:20:04 +0300

    feat: fix service

--- lib/src/core/data/service/contact_service/contact_service.dart
+++ lib/src/core/data/service/contact_service/contact_service.dart
@@ -5,10 +5,6 @@ import '../../model/user.dart';
 
 abstract class ContactService {
   Future<List<Contacts>> getUsers(User user);
-  Future<bool> addUserWithImage(
-    User user,
-    Contacts contacts,
-    File? imageFile,
-  );
+  Future<bool> addUserWithImage(User user, Contacts contacts, File? imageFile);
   Future<bool> deleteContact(User user, Contacts contacts);
 }
