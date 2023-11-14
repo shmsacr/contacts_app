3d553e6d01e5faeeff4cf5c726326b0413b4204f
shmsacr <shmsacr@gmail.com>
2023-11-14 20:17:06 +0300

    feat: add contact event pagenation

--- lib/src/core/bloc/contacts_bloc/contacts_event.dart
+++ lib/src/core/bloc/contacts_bloc/contacts_event.dart
@@ -1,34 +1,46 @@
 import 'dart:io';
 
 import 'package:equatable/equatable.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart' show immutable;
 
 import '../../data/model/contacts.dart';
 
 @immutable
-abstract class ContactsEvent extends Equatable {}
-
-class LoadUserEvent extends ContactsEvent {
+abstract class ContactsEvent extends Equatable {
+  const ContactsEvent();
   @override
   List<Object?> get props => [];
 }
 
-class ResetUserEvent extends ContactsEvent {
+class ContactsFetch extends ContactsEvent {
+  const ContactsFetch();
   @override
   List<Object?> get props => [];
 }
 
-class PostUserEvent extends ContactsEvent {
-  Contacts data;
-  File? file;
-  PostUserEvent({required this.data, this.file});
+class LoadContactEvent extends ContactsEvent {
+  final int page;
+  LoadContactEvent({this.page = 1});
+  @override
+  List<Object?> get props => [page];
+}
+
+class ResetContactEvent extends ContactsEvent {
+  @override
+  List<Object?> get props => [];
+}
+
+class PostContactEvent extends ContactsEvent {
+  final Contacts data;
+  final File? file;
+  PostContactEvent({required this.data, this.file});
   @override
   List<Object?> get props => [data];
 }
 
-class DeleteUserEvent extends ContactsEvent {
-  Contacts data;
-  DeleteUserEvent({required this.data});
+class DeleteContactEvent extends ContactsEvent {
+  final Contacts data;
+  DeleteContactEvent({required this.data});
   @override
   List<Object?> get props => [data];
 }
