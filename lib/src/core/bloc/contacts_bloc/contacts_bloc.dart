dc8b597a72b5388e5df4882068bd989a62459f45
shmsacr <shmsacr@gmail.com>
2023-11-14 20:16:52 +0300

    feat: add contact bloc pagenation

--- lib/src/core/bloc/contacts_bloc/contacts_bloc.dart
+++ lib/src/core/bloc/contacts_bloc/contacts_bloc.dart
@@ -1,38 +1,34 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:contacts_app/src/core/bloc/contacts_bloc/contacts_event.dart';
 import 'package:contacts_app/src/core/bloc/contacts_bloc/contacts_state.dart';
 import 'package:contacts_app/src/core/data/service/contact_service/contact_service_impl.dart';
+import 'package:stream_transform/stream_transform.dart';
 
 import '../../data/repository/database/database_repository.dart';
 import '../../data/repository/database/database_repository_impl.dart';
 import '../../data/service/contact_service/contact_service.dart';
 
+const _contactDuration = Duration(milliseconds: 300);
+EventTransformer<T> contactDroppable<T>(Duration duration) {
+  return (events, mapper) {
+    return droppable<T>().call(events.throttle(duration), mapper);
+  };
+}
+
 class ContactsBloc extends Bloc<ContactsEvent, ContactsState> {
   ContactsBloc() : super(ContactsLoadingState()) {
-    on<LoadUserEvent>(_loadUserEvent);
-    on<PostUserEvent>(_postUserEvent);
-    on<ResetUserEvent>(_resetUserEvent);
-    on<DeleteUserEvent>(_deleteUserEvent);
-  }
-
-  FutureOr<void> _loadUserEvent(
-      LoadUserEvent event, Emitter<ContactsState> emit) async {
-    try {
-      emit(ContactsLoadingState());
-      final ContactService _contactService = ContactServiceImpl();
-      final DatabaseRepository _userDatabaseProvider = DatabaseRepositoryImpl();
-      final user = await _userDatabaseProvider.getUserById(1);
-      final users = await _contactService.getUsers(user!);
-      emit(ContactsLoadedState(users));
-    } catch (e) {
-      emit(ContactsErrorState(e.toString()));
-    }
+    on<ContactsFetch>(_contactFetch,
+        transformer: contactDroppable(_contactDuration));
+    on<PostContactEvent>(_postUserEvent);
+    on<ResetContactEvent>(_resetUserEvent);
+    on<DeleteContactEvent>(_deleteUserEvent);
   }
 
   Future<FutureOr<void>> _postUserEvent(
-      PostUserEvent event, Emitter<ContactsState> emit) async {
+      PostContactEvent event, Emitter<ContactsState> emit) async {
     try {
       final ContactService _contactService = ContactServiceImpl();
       final DatabaseRepository _userDatabaseProvider = DatabaseRepositoryImpl();
@@ -41,7 +37,7 @@ class ContactsBloc extends Bloc<ContactsEvent, ContactsState> {
           await _contactService.addUserWithImage(user!, event.data, event.file);
       if (postUser) {
         emit(ContactsSuccessState());
-        await _loadUserEvent(LoadUserEvent(), emit);
+        await _contactFetch(ContactsFetch(), emit);
       } else {
         emit(ContactsErrorState("Kullanıcı eklenemedi"));
       }
@@ -50,12 +46,12 @@ class ContactsBloc extends Bloc<ContactsEvent, ContactsState> {
     }
   }
 
-  void _resetUserEvent(ResetUserEvent event, Emitter<ContactsState> emit) {
+  void _resetUserEvent(ResetContactEvent event, Emitter<ContactsState> emit) {
     emit(ContactsLoadingState());
   }
 
   FutureOr<void> _deleteUserEvent(
-      DeleteUserEvent event, Emitter<ContactsState> emit) async {
+      DeleteContactEvent event, Emitter<ContactsState> emit) async {
     try {
       final ContactService _contactService = ContactServiceImpl();
       final DatabaseRepository _userDatabaseProvider = DatabaseRepositoryImpl();
@@ -63,12 +59,43 @@ class ContactsBloc extends Bloc<ContactsEvent, ContactsState> {
       final deleteUser = await _contactService.deleteContact(user!, event.data);
       if (deleteUser) {
         emit(ContactsLoadingState());
-        await _loadUserEvent(LoadUserEvent(), emit);
+        await _contactFetch(ContactsFetch(), emit);
       } else {
         emit(ContactsErrorState("Kullanıcı silinemedi"));
       }
     } catch (e) {
       emit(ContactsErrorState(e.toString()));
+    }
+  }
+
+  FutureOr<void> _contactFetch(
+      ContactsFetch event, Emitter<ContactsState> emit) async {
+    final ContactService _contactService = ContactServiceImpl();
+    final DatabaseRepository _userDatabaseProvider = DatabaseRepositoryImpl();
+    final user = await _userDatabaseProvider.getUserById(1);
+    if (state.hasReachedMax) {
+      return null;
+    }
+    try {
+      if (state.status == ContactStatus.initial) {
+        final contact = await _contactService.getUsers(user!);
+        print(state.contact.length);
+        emit(state.copyWith(
+            contact: contact,
+            hasReachedMax: false,
+            status: ContactStatus.success,
+            page: state.page));
+      }
+      final contact = await _contactService.getUsers(user!, state.page + 1);
+      contact.isEmpty
+          ? emit(state.copyWith(hasReachedMax: true))
+          : emit(state.copyWith(
+              contact: List.of(state.contact)..addAll(contact),
+              hasReachedMax: false,
+              status: ContactStatus.success,
+              page: state.page + 1));
+    } catch (e) {
+      emit(state.copyWith(status: ContactStatus.failure));
     }
   }
 }
