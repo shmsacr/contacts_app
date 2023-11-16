f835a63136a8bbf3bae836fda414649e711f259b
shmsacr <shmsacr@gmail.com>
2023-11-16 23:30:52 +0300

    feat: add image max size

--- lib/src/core/data/service/contact_service/contact_service_impl.dart
+++ lib/src/core/data/service/contact_service/contact_service_impl.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http_parser/http_parser.dart' show MediaType;
+import 'package:image/image.dart' as Img;
 
 import '../../model/contacts.dart';
 import '../../model/user.dart';
@@ -43,18 +44,30 @@ class ContactServiceImpl implements ContactService {
     File? imageFile,
   ) async {
     String? fileName = imageFile?.path.split('/').last;
-    FormData? formData;
+    FormData _formData;
     try {
       if (imageFile != null) {
-        formData = FormData.fromMap({
-          'resim': await MultipartFile.fromFile(imageFile.path,
-              filename: fileName, contentType: MediaType('image', 'jpg')),
+        final resizedImage = Img.copyResize(
+          Img.decodeImage(imageFile!.readAsBytesSync())!,
+          width: 100,
+          height: 100,
+        );
+        final resizedImageData = Img.encodeJpg(resizedImage);
+        final resizedMultipartFile = MultipartFile.fromBytes(
+          resizedImageData,
+          filename: fileName,
+          contentType: MediaType('image', 'jpg'),
+        );
+
+        _formData = FormData.fromMap({
+          'resim': resizedMultipartFile,
         });
       } else {
-        formData = FormData.fromMap({
+        _formData = FormData.fromMap({
           'resim': null,
         });
       }
+
       Response response = await dio.post(
         'http://www.motosikletci.com/api/kisi-kaydet',
         queryParameters: {
@@ -66,7 +79,7 @@ class ContactServiceImpl implements ContactService {
           'town_id': contacts.town_id,
           'kisi_tel': contacts.kisi_tel,
         },
-        data: formData,
+        data: _formData,
       );
       if (response.statusCode == 200) {
         var responseData = response.data;
@@ -96,7 +109,6 @@ class ContactServiceImpl implements ContactService {
     if (response.statusCode == 200) {
       var responseData = response.data;
       if (responseData["basari"] == 1 && responseData["durum"] == 3) {
-        print('User data and image uploaded successfully: ${response.data}');
         return true;
       } else {
         throw ('Error uploading user data: ${response.data}');
